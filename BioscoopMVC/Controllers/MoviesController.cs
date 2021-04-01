8cb18ba91b888cf3fa8fd4a3dd7bb7cbdc613c4a
Daniël R <danielroth1890@gmail.com>
2021-04-01 23:17:26 +0200

    Add project files.

---
+++ BioscoopMVC/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace BioscoopMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // 
+        // GET: /Movie/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /Movie/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
+        }
+    }
+}
---
+++ BioscoopMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BioscoopMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
